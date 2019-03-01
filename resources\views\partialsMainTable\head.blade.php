b08f6ea3da0031d14513905bc5a0f814c8dd11e0
alipourmehdi <47037762+alipourmehdi@users.noreply.github.com>
2019-03-01 23:29:34 +0100

    head.blade.php
    header file containing css links in the folder  resources\views\partialsMainTable\head.blade.php need to be changed otherwise css classes wont be loaded in the front-end . corrected links should be loaded using href="{{ URL::asset('/css/style.css') }}" which css folder is within public folder.

---
+++ resources\views\partialsMainTable\head.blade.php
@@ -0,0 +1,33 @@
+  <!-- SITE TITTLE -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Laravel Classimax</title>
+
+  <!-- PLUGINS CSS STYLE -->
+  <link href="{{ URL::asset('/adminlte/plugins/jQueryUI/jquery-ui.min.css') }}" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link href="{{ URL::asset('/adminlte/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link href="{{ URL::asset('/adminlte/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+  <!-- Owl Carousel -->
+  <link href="{{ URL::asset('/adminlte/plugins/slick-carousel/slick/slick.css') }}" rel="stylesheet">
+  <link href="{{ URL::asset('/adminlte/plugins/slick-carousel/slick/slick-theme.css') }}" rel="stylesheet">
+  <!-- Fancy Box -->
+  <link href="{{ URL::asset('/adminlte/plugins/fancybox/jquery.fancybox.pack.css') }}" rel="stylesheet">
+  <link href="{{ URL::asset('/adminlte/plugins/jquery-nice-select/css/nice-select.css') }}" rel="stylesheet">
+  <link href="{{ URL::asset('/adminlte/plugins/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css') }}" rel="stylesheet">
+  <!-- CUSTOM CSS   -->
+
+
+ <link rel="stylesheet" href="{{ URL::asset('/css/style.css') }}">
+
+  <!-- FAVICON -->
+  <link href="{{ URL::asset('/img/favicon.png') }}" rel="shortcut icon">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
